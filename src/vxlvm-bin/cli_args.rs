4f8ab3128750a220bf4f4d51cf01184ccf2d8c77
aidos9 <20310468+aidos9@users.noreply.github.com>
2022-01-01 12:25:04 +1100

    Removed unused attribute

--- src/vxlvm-bin/cli_args.rs
+++ src/vxlvm-bin/cli_args.rs
@@ -5,6 +5,5 @@ use clap::Parser;
 #[clap(about = "The virtual machine for executing xvl files.")]
 pub struct CLIArgs {
     /// The file to execute
-    #[clap(min_values = 1)]
     pub input_file: String,
 }
